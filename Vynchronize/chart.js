4f683c08ecd0096612399526f5dc933817b7fff2
Yaroslav <yarik300895@gmail.com>
2019-12-01 18:25:06 +0200

    changes code

--- Vynchronize/chart.js
+++ Vynchronize/chart.js
@@ -29,17 +29,42 @@ function start(){
         data.addColumn('string', 'Розроблюваний продукт');
         data.addColumn('number', 'Значення параметру');
         data.addRows([
-          ['Продуктивність', a2],
-          ['Якість', b2],
-          ['Питома вартість', c2],
-          ['Документованість', d2],
-		  ['Документованість 2', 0.0010]
+          ['Продуктивність', a2]
+          //['Якість', b2],
+         // ['Питома вартість', c2],
+         // ['Документованість', d2],
+		  //['Документованість 2', 0.0010]
         ]);
 
+		var data2 = new google.visualization.DataTable();
+        data2.addColumn('string', 'Розроблюваний продукт');
+        data2.addColumn('number', 'Значення параметру');
+        data2.addRows([
+          ['Якість', b2]
+        ]);
+		
+		var data3 = new google.visualization.DataTable();
+        data3.addColumn('string', 'Розроблюваний продукт');
+        data3.addColumn('number', 'Значення параметру');
+        data3.addRows([
+          ['Питома вартість', c2]
+         ]);
+		
+		var data4 = new google.visualization.DataTable();
+        data4.addColumn('string', 'Розроблюваний продукт');
+        data4.addColumn('number', 'Значення параметру');
+        data4.addRows([
+          ['Документованість', d2]
+        ]);
+		
         // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':800,
-                       'height':450};
+        var options = {'title':'Графік по параметру',
+                       'width':278,
+                       'height':550,
+					   bar: {groupWidth: "7%"},
+					   hAxis: {minValue: 0.1, maxValue: 200}, 
+					   vAxis: {minValue: 0.1, maxValue: 200} 			   
+ };
 options.explorer = {
   //maxZoomIn: [0.1]
   actions: ['dragToZoom', 'rightClickToReset']
@@ -48,8 +73,25 @@ options.explorer = {
         // Instantiate and draw our chart, passing in some options.
         var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
         chart.draw(data, options);
+		
+		var chart2 = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
+        chart2.draw(data2, options);
+		
+		var chart3 = new google.visualization.ColumnChart(document.getElementById('chart_div3'));
+        chart3.draw(data3, options);
+		
+		var chart4 = new google.visualization.ColumnChart(document.getElementById('chart_div4'));
+        chart4.draw(data4, options);
+		
       }
+	  
+var fso, f1;
+fso = new ActiveXObject("Scripting.FileSystemObject");
+f1 = fso.CreateTextFile("c:\\testfile.txt", true);
+f1.Write ("This is a test."); 
+
+	 
 }  
-	  setTimeout(start, 9000);
+	  setTimeout(start, 2000);
 	  
 	  
